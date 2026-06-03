33fd2ba44c9e9b315efdf932d8a32de53f57f018
D-Willz <w.dampierjr@gmail.com>
2026-06-02 18:22:39 -0700

    Update ads.txt.ts

--- src/routes/ads.txt.ts
+++ src/routes/ads.txt.ts
@@ -1,14 +1,1 @@
-import { createServerFileRoute } from '@tanstack/react-start/server'
 
-export const ServerRoute = createServerFileRoute('/ads.txt').methods({
-  GET: () => {
-    return new Response(
-      'google.com, pub-6436860193358277, DIRECT, f08c47fec0942fa0',
-      {
-        headers: {
-          'Content-Type': 'text/plain',
-        },
-      }
-    )
-  },
-})
